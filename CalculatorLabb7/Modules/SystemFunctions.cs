d3ad37b77277b401507f928f9565de72009f1a4e
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2023-06-27 01:00:55 +0200

    added a comment

--- CalculatorLabb7/Modules/SystemFunctions.cs
+++ CalculatorLabb7/Modules/SystemFunctions.cs
@@ -8,14 +8,18 @@ namespace CalculatorLabb7.Modules {
 
     public class SystemFunctions {
 
+        //Can be read from outside of this class but can only be modified here
         public static List<string> CalculatorList { get; private set; } = new List<string>();
+
         public void LogCalcutlator(double num1, double num2, char oprtr, double res) {
+            //Style of the information saved to the list
             string log = $"{num1} {oprtr} {num2} = {res}";
             CalculatorList.Add(log);
             Console.WriteLine(log);
         }
 
         public void PrintLogs() {
+            //For each item/line in the list print
             foreach (string item in CalculatorList) {
                 Console.WriteLine(item);
             }
